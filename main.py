55c3b17ab4c54288474629d93d032d7b7e395f84
aliali <alihkime20@gmail.com>
2026-08-01 02:27:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import asyncio
+from aiogram import Bot, Dispatcher
 
+from config import BOT_TOKEN
+from database import init_db
+
+
+async def main():
+    # إنشاء قاعدة البيانات
+    init_db()
+
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    print("Bot is running...")
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
