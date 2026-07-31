d28e6ffe407450afdb71910bb9b24e7d297a84bf
aliali <alihkime20@gmail.com>
2026-08-01 02:29:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import init_db
 
+from handlers import start, admin, seller
+
 
 async def main():
-    # إنشاء قاعدة البيانات
     init_db()
 
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
+
+    dp.include_router(start.router)
+    dp.include_router(admin.router)
+    dp.include_router(seller.router)
 
     print("Bot is running...")
 
