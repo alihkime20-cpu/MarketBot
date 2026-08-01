59975dc17af9be93877abbb2f511de12052e4047
aliali <alihkime20@gmail.com>
2026-08-01 19:23:19 +0300

    Update users.py

--- services/users.py
+++ services/users.py
@@ -1,1 +1,46 @@
+from database import get_connection
 
+
+def add_user(telegram_id: int, name: str, username: str | None):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT id FROM users WHERE telegram_id = ?",
+        (telegram_id,)
+    )
+
+    user = cursor.fetchone()
+
+    if not user:
+        cursor.execute(
+            """
+            INSERT INTO users (telegram_id, name, username, role)
+            VALUES (?, ?, ?, ?)
+            """,
+            (
+                telegram_id,
+                name,
+                username,
+                "buyer"
+            )
+        )
+        conn.commit()
+
+    conn.close()
+
+
+def get_user(telegram_id: int):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE telegram_id = ?",
+        (telegram_id,)
+    )
+
+    user = cursor.fetchone()
+
+    conn.close()
+
+    return user
